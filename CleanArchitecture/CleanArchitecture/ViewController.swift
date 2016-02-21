52016083432e4880c0ff91140e2da985a409b9db
Marcin Kuptel <marcinkuptel@gmail.com>
2016-02-21 21:15:11 +0100

    Initial commit.

---
+++ CleanArchitecture/CleanArchitecture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanArchitecture
+//
+//  Created by Marcin Kuptel on 21/02/2016.
+//  Copyright © 2016 Marcin Kuptel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
