fb9ba435c3867d94a463baaf230139237a0fbece
Marcin Kuptel <marcinkuptel@gmail.com>
2016-02-21 21:32:55 +0100

    Added FilmInteractor class.

---
+++ CleanArchitecture/CleanArchitecture/Source/Films/Interactor/FilmInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  FilmInteractor.swift
+//  CleanArchitecture
+//
+//  Created by Marcin Kuptel on 21/02/2016.
+//  Copyright © 2016 Marcin Kuptel. All rights reserved.
+//
+
+import Foundation
+
+class FilmInteractor {
+    
+}
