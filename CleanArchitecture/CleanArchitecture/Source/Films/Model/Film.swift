5e5f4a63778468eea0c9349f7f84c599e03712ec
Marcin Kuptel <marcinkuptel@gmail.com>
2016-02-21 21:53:10 +0100

    Added Film struct.

---
+++ CleanArchitecture/CleanArchitecture/Source/Films/Model/Film.swift
@@ -0,0 +1,13 @@
+//
+//  Film.swift
+//  CleanArchitecture
+//
+//  Created by Marcin Kuptel on 21/02/2016.
+//  Copyright © 2016 Marcin Kuptel. All rights reserved.
+//
+
+import Foundation
+
+struct Film {
+    
+}
