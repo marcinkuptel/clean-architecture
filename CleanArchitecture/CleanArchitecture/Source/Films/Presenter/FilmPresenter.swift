4dfea76d7c315898ef11cc9229851d558291bb9a
Marcin Kuptel <marcinkuptel@gmail.com>
2016-02-21 21:23:35 +0100

    Added FilmPresenter class.

---
+++ CleanArchitecture/CleanArchitecture/Source/Films/Presenter/FilmPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  FilmPresenter.swift
+//  CleanArchitecture
+//
+//  Created by Marcin Kuptel on 21/02/2016.
+//  Copyright © 2016 Marcin Kuptel. All rights reserved.
+//
+
+import Foundation
